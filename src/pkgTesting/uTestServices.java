b36d2c0956960e27789b1632039133a5e09d7e8a
santi <santi@Santiago>
2026-09-13 14:25:28 -0500

    Commit Testion MicroKernel

--- src/pkgTesting/uTestServices.java
+++ src/pkgTesting/uTestServices.java
@@ -5,9 +5,11 @@
 package pkgTesting;
 
 import pkgDomain.clsController;
+import pkgDomain.clsUser;
 import pkgServices.pkgDataBase.clsQuestionDao;
 import pkgServices.pkgDataBase.clsRoleDao;
 import pkgServices.pkgDataBase.clsUserDao;
+import pkgServices.pkgMicrokernel.clsQuestionMicrokernel;
 
 /**
  *
@@ -15,35 +17,61 @@ import pkgServices.pkgDataBase.clsUserDao;
  */
 public class uTestServices {
 
-    public static void main(String[] args) {
-// 1. Rol: Administrador
-        clsRoleDao.opSaveRole("ROL-001", "Administrador", "");
+//    public static void main(String[] args) {
+    //// 1. Rol: Administrador
+//        clsRoleDao.opSaveRole("ROL-001", "Administrador", "");
+//
+//// 2. Rol: Autor de preguntas
+//        clsRoleDao.opSaveRole("ROL-002", "Autor de preguntas", "");
+//
+//// 3. Rol: Revisor
+//        clsRoleDao.opSaveRole("ROL-003", "Revisor", "");
+//
+//// 4. Rol: Docente
+//        clsRoleDao.opSaveRole("ROL-004", "Docente", "");
+//
+//// 5. Rol: Estudiante
+//        clsRoleDao.opSaveRole("ROL-005", "Estudiante", "");
+//// 6. Question: prueba
+//        clsQuestionDao.opSaveQuestion(
+//                "PREG-001", // prmIdQuestion
+//                "Leyes de Inducción", // prmQuestionName
+//                "¿Qué ley establece que la FEM inducida es proporcional a la variación del flujo magnético?", // prmQuestionDescription
+//                "Ley de Ampère", // prmOptionA
+//                "Ley de Faraday", // prmOptionB (Correcta)
+//                "Ley de Ohm", // prmOptionC
+//                "Ley de Gauss", // prmOptionD
+//                "B", // prmRightAnswer
+//                "ACTIVA", // prmState
+//                "51412c00-91f7-454e-a6c9-aff7c1c44dd6",
+//                "",
+//                ""// prmIdUsuario (admin test)
+//        );
+//    }
+        
+        public static void main(String[] args) {
 
-// 2. Rol: Autor de preguntas
-        clsRoleDao.opSaveRole("ROL-002", "Autor de preguntas", "");
+       
+        clsUser varUser = new clsUser();
+        clsQuestionMicrokernel varObject = new clsQuestionMicrokernel();
+        
+        System.out.println("Test Multiple Selection Question: ");
+        varObject.opProcessQuestion("SELECCION_MULTIPLE", "10000", "Pre", "Una pregunta Test", "Test option 1", "Test option 2", "Test option 3", "Test option 4", "option 4", "Aprobado", "TipoTest", "AddressUrl", varUser);
 
-// 3. Rol: Revisor
-        clsRoleDao.opSaveRole("ROL-003", "Revisor", "");
+        System.out.println("Test Case Question ");
+        varObject.opProcessQuestion("TIPO_CASO", "10005", "Pre", "Una pregunta Test", "Test option 1", "Test option 2", "Test option 3", "Test option 4", "option 4", "Aprobado", "TipoTest", "AddressUrl", varUser);
 
-// 4. Rol: Docente
-        clsRoleDao.opSaveRole("ROL-004", "Docente", "");
+        
+        System.out.println("Test Multimedia Question ");
+        varObject.opProcessQuestion("MULTIMEDIA", "10004", "Pre", "Una pregunta Test", "Test option 1", "Test option 2", "Test option 3", "Test option 4", "option 4", "Aprobado", "TipoTest", "AddressUrl", varUser);
+        
+         System.out.println("Test Fallo");
+        varObject.opProcessQuestion(";alskjdf;alskjdf", "10009", "Pre", "Una pregunta Test", "Test option 1", "Test option 2", "Test option 3", "Test option 4", "option 4", "Aprobado", "TipoTest", "AddressUrl", varUser);
+        
+        
+        
 
-// 5. Rol: Estudiante
-        clsRoleDao.opSaveRole("ROL-005", "Estudiante", "");
-// 6. Question: prueba
-        clsQuestionDao.opSaveQuestion(
-                "PREG-001", // prmIdQuestion
-                "Leyes de Inducción", // prmQuestionName
-                "¿Qué ley establece que la FEM inducida es proporcional a la variación del flujo magnético?", // prmQuestionDescription
-                "Ley de Ampère", // prmOptionA
-                "Ley de Faraday", // prmOptionB (Correcta)
-                "Ley de Ohm", // prmOptionC
-                "Ley de Gauss", // prmOptionD
-                "B", // prmRightAnswer
-                "ACTIVA", // prmState
-                "51412c00-91f7-454e-a6c9-aff7c1c44dd6",
-                "",
-                ""// prmIdUsuario (admin test)
-        );
     }
+;
+
 }
